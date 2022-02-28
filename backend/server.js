f94a3dfd7f0c7d2f9f3a0f83356b2f123b7e317e
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2022-02-28 21:21:36 +0600

    initializing project (again)

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(`Selected ID is:`, req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}\n`));
