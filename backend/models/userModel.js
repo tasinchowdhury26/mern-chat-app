0b33a0615fd1be617b3774a36ec2e680d41cab60
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2022-03-08 10:38:37 +0600

    heading to signup-login (frontend)

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs/dist/bcrypt");
 
 const userSchema = mongoose.Schema(
   {
@@ -15,6 +16,20 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//before saving user to db, encrypt the password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10); //higher the number, stronger the password
+  this.password = await bcrypt.hash(this.password, salt); // hash the password with the generated salt
+});
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,22 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
 const app = express();
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to accept JSON data from the frontend
+app.use(notFound);
+app.use(errorHandler);
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use("/api/users", userRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`.yellow.bold));
